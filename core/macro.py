6443e873ed68fef48c64d1a2c0879c50eaea760e
Angus Hollands <goosey15@gmail.com>
2017-01-05 17:04:57 +0000

    Switch dict constructor to literal

--- core/macro.py
+++ core/macro.py
@@ -224,12 +224,7 @@ class Macro:
             return
 
         if isinstance(type, tuple) or isinstance(type, str):
-            self._type_args = dict(
-             _order=["_arg1"],
-             _required=["_arg1"],
-             _default={},
-             _arg1=type,
-            )
+            self._type_args = {'_order': ["_arg1"], '_required': ["_arg1"], '_default': {}, '_arg1': type}
             return
 
         if not isinstance(type, dict):
@@ -245,7 +240,7 @@ class Macro:
             assert sorted(order) == sorted([k for k in type.keys() if not k.startswith("_")])
         required = set()
         default = {}
-        type_args = dict(_order=order, _required=required, _default=default)
+        type_args = {'_order': order, '_required': required, '_default': default}
         last_non_required_name = None
 
         for name, value in type.items():
