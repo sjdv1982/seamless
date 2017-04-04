4fd8699e4f4974e1002c17ec890e8415bc0e11e9
Angus Hollands <goosey15@gmail.com>
2017-04-04 22:42:41 +0100

    Cleanups

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,9 @@
 import weakref
 
+
 class SeamlessBase:
     _destroyed = False
-    _macro_object = None # macro object that CREATED this instance
+    _macro_object = None  # macro object that CREATED this instance
     _context = None
     _last_context = None
     name = None
@@ -27,41 +28,47 @@ class SeamlessBase:
 
     def _find_successor(self):
         path = list(self.path)
-        p = self
+        node = self
         subpath = []
-        ok = False
-        while p._destroyed:
-            if not len(path):
+
+        while node._destroyed:
+            if not path:
                 break
-            p = p._last_context
-            if p is None:
+
+            node = node._last_context
+            if node is None:
                 break
+
             subpath = [path.pop(-1)] + subpath
-        if not p._destroyed:
+
+        if not node._destroyed:
             for subp in subpath:
                 try:
-                    p = getattr(p, subp)
-                    assert not p._destroyed
+                    node = getattr(node, subp)
+                    assert not node._destroyed
                 except:
                     break
+
             else:
-                ok = True
-        if ok:
-            return p
+                return node
 
     def _set_context(self, context, name, force_detach=False):
         from .context import Context
         assert isinstance(context, Context)
-        if self._context is not None:
+        self_context = self._context
+
+        if self_context is not None:
             assert self.name is not None
-            if context is not self._context or force_detach:
-                childname = self.name
-                assert self._context._children[childname] is self
-                self._context._children.pop(childname)
-                if childname in self._context._auto:
-                    self._context._auto.remove(childname)
+            if context is not self_context or force_detach:
+                child_name = self.name
+                assert self_context._children[child_name] is self
+                self_context._children.pop(child_name)
+                if child_name in self_context._auto:
+                    self_context._auto.remove(child_name)
+
         if context is not None:
             self._last_context = context
+
         self._context = context
         self.name = name
         return self
@@ -93,7 +100,7 @@ class SeamlessBase:
             obj._owner = None
             macro_control = self._macro_control()
             if not get_macro_mode() and \
-              macro_control is not None and macro_control is not obj._macro_control():
+                            macro_control is not None and macro_control is not obj._macro_control():
                 macro_cells = macro_control._macro_object.cell_args.values()
                 macro_cells = sorted([str(c) for c in macro_cells])
                 macro_cells = "\n  " + "\n  ".join(macro_cells)
@@ -102,73 +109,75 @@ class SeamlessBase:
 WARNING: {0} is now owned by {1}, which is under live macro control.
 The macro is controlled by the following cells: {2}
 When any of these cells change and the macro is re-executed, the owned object will be deleted and likely not re-created
-***********************************************************************************************************************"""\
-                    .format(obj, self, macro_cells))
+***********************************************************************************************************************""" \
+                          .format(obj, self, macro_cells))
                 elif macro_control is not None:
                     print("""***********************************************************************************************************************
 WARNING: {0} is now owned by {1}, which is a child of, or owned by, {2}, which is under live macro control.
 The macro is controlled by the following cells: {3}
 When any of these cells change and the macro is re-executed, the owned object will be deleted and likely not re-created
-***********************************************************************************************************************"""\
-                    .format(obj, self, macro_control, macro_cells))
+***********************************************************************************************************************""" \
+                          .format(obj, self, macro_control, macro_cells))
             obj._owner = weakref.ref(self)
 
     def _owns_all(self):
-        owns = set(self._owned)
-        for owned in self._owned:
-            owns.update(owned._owns_all())
-        return owns
+        # TODO instead, can the leaf owns_all just include self in owns list?
+        return set(self._owned) | {o._owns_all() for o in self._owned}
 
     def _macro_control(self, include_owner=False, primary=True, done=None):
         if self._macro_object is not None:
             return self
+
         if done is None:
             done = []
+
         if self in done:
             msg = "Ownership circle:\n    " + "\n    ".join([str(x) for x in done])
             raise Exception(msg)
+
         done.append(self)
 
-        ret = None
         if self.context is not None:
-            ret = self.context._macro_control(include_owner, False, done)
-        if ret is not None:
-            return ret
-        if include_owner:
-            if self._owner is not None:
-                owner = self._owner()
-                if owner is not None:
-                    ret = owner._macro_control(True, False)
-                    if ret is not None:
-                        return ret
+            control = self.context._macro_control(include_owner, False, done)
+            if control is not None:
+                return control
+
+        if include_owner and self._owner is not None:
+            owner = self._owner()
+            if owner is not None:
+                control = owner._macro_control(True, False)
+                if control is not None:
+                    return control
+
         elif primary:
             return self._macro_control(True, True)
         else:
             return None
 
-
     def destroy(self):
         if self._destroyed:
             return
         self._destroyed = True
+
         ctx = self._context
         if ctx is not None:
-            for childname, child in ctx._children.items():
+            for name, child in ctx._children.items():
                 if child is self:
-                    ctx._children.pop(childname)
+                    ctx._children.pop(name)
                     break
+
             ctx._manager.unstable_processes.discard(self)
+
         for obj in self._owned:
             obj.destroy()
 
-
     def __str__(self):
-        ret = "." + ".".join(self.path)
+        text = "." + ".".join(self.path)
         if self._owner is not None:
             owner = self._owner()
             if owner is not None:
-                ret += ", owned by " + str(owner)
-        return ret
+                text = "{}, owned by {}".format(text, owner)
+        return text
 
     def __repr__(self):
         return self.__str__()
@@ -176,11 +185,12 @@ When any of these cells change and the macro is re-executed, the owned object wi
     @property
     def macro(self):
         return self._macro_object
+
     def _set_macro_object(self, macro_object):
         self._macro_object = macro_object
 
     def __del__(self):
-        #print("__del__", type(self), self.path, self._destroyed)
+        # print("__del__", type(self), self.path, self._destroyed)
         try:
             self.destroy()
         except:
