14a38c3fc6954b4d6b5774c5af1d59ec7f3fd71d
Angus Hollands <goosey15@gmail.com>
2017-08-12 18:00:36 +0100

    Add other tools to setup.py

---
+++ seamless/tools/print_xml.py
@@ -0,0 +1,5 @@
+from __future__ import print_function
+import sys
+from lxml import etree, objectify
+xml = objectify.parse(open(sys.argv[1]))
+print(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode())
