5872d0bff1484a84e1462e596dcbbf8db27b2b04
Angus Hollands <goosey15@gmail.com>
2017-08-12 18:22:13 +0100

    Make seamless command use argpase Bugfix __file__ string

--- seamless/tools/seamless.py
+++ seamless/tools/seamless.py
@@ -1,31 +1,49 @@
 #!/usr/bin/env python3
 
 import sys
+from argparse import ArgumentParser
 
 
-def main():
-    cmd = """import seamless
+
+SCRIPT_FILE_TEMPLATE = """ctx = context()
+__file__ = {file_path!r}
+{file_contents}
+"""
+
+SCRIPT_TEMPLATE = """import seamless
 from seamless import context, cell, pythoncell, transformer, reactor, \
  macro, export
 from seamless.lib import link, edit, display
 from seamless.gui import shell
+
+{body}
 """
-    if len(sys.argv) == 1:
-        cmd += "ctx = context()"
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("file_path", default=None, nargs='?')
+    args = parser.parse_args()
+    file_path = args.file_path
+
+    if file_path is None:
+        program = SCRIPT_TEMPLATE.format(body="ctx = context()")
 
     else:
-        assert len(sys.argv) == 2  # syntax: seamless file.seamless
-        f = sys.argv[1]
-        if f.endswith(".seamless"):
-            cmd += "ctx = seamless.fromfile('{0}')".format(f)
-        else:
-            cmd += "ctx = context()\n"
-            cmd += "__file__ = '{0}'\n".format(f)
-            cmd += open(f).read()
-    sys.argv = [sys.argv[0], "-c", cmd, "-i"]
+        if file_path.endswith(".seamless"):
+            program = SCRIPT_TEMPLATE.format(body="ctx = seamless.fromfile('{}')".format(file_path))
 
+        else:
+            with open(file_path) as f:
+                file_contents = f.read()
+
+            program = SCRIPT_TEMPLATE.format(
+                body=SCRIPT_FILE_TEMPLATE.format(file_path=file_path, file_contents=file_contents)
+            )
+
+    argv = [sys.argv[0], "-c", program, "-i"]
+    # TODO use tempfile
     from IPython import start_ipython
-    sys.exit(start_ipython())
+    sys.exit(start_ipython(argv))
 
 
 if __name__ == "__main__":
