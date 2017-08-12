54965b725b242eb0d07a2c58ba82be14f4286235
Angus Hollands <goosey15@gmail.com>
2017-08-12 18:24:59 +0100

    Bugfixes

--- seamless/tools/parse_silk.py
+++ seamless/tools/parse_silk.py
@@ -3,11 +3,13 @@ import sys
 from seamless.silk.typeparse import parse
 from lxml import etree
 
-silktext = open(sys.argv[1]).read()
-tree = parse(silktext)
-print(etree.tostring(
-    tree,
-    pretty_print=True,
-    xml_declaration=True,
-    encoding="UTF-8").decode()
-)
+
+def main():
+    silktext = open(sys.argv[1]).read()
+    tree = parse(silktext)
+    print(etree.tostring(
+        tree,
+        pretty_print=True,
+        xml_declaration=True,
+        encoding="UTF-8").decode()
+    )
--- seamless/tools/print_xml.py
+++ seamless/tools/print_xml.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import sys
 from lxml import etree, objectify
-xml = objectify.parse(open(sys.argv[1]))
-print(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode())
+
+def main():
+    xml = objectify.parse(open(sys.argv[1]))
+    print(etree.tostring(xml, pretty_print=True, xml_declaration=True, encoding="UTF-8").decode())
