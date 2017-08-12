3c56c5c1754771257ada54ede1a1a28bc46ade4c
Angus Hollands <goosey15@gmail.com>
2017-08-12 19:10:48 +0100

    Use temp file for readability

--- seamless/tools/seamless.py
+++ seamless/tools/seamless.py
@@ -1,9 +1,21 @@
 import sys
+from contextlib import contextmanager
 from argparse import ArgumentParser
+from tempfile import mkstemp
+from os import remove, write, close
+from pathlib import Path
+
+
+@contextmanager
+def temp_file():
+    fd, temp_file_path = mkstemp()
+    yield temp_file_path
+    close(fd)
+    remove(temp_file_path)
 
 
 SCRIPT_FILE_TEMPLATE = """ctx = context()
-__file__ = {file_path!r}
+__file__ = '{file_path}'
 {file_contents}
 """
 
@@ -18,7 +30,7 @@ from seamless.gui import shell
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("file_path", default=None, nargs='?')
+    parser.add_argument("file_path", type=Path, default=None, nargs='?')
     args = parser.parse_args()
     file_path = args.file_path
 
@@ -26,21 +38,25 @@ def main():
         program = SCRIPT_TEMPLATE.format(body="ctx = context()")
 
     else:
-        if file_path.endswith(".seamless"):
-            program = SCRIPT_TEMPLATE.format(body="ctx = seamless.fromfile('{}')".format(file_path))
+        if file_path.suffix == ".seamless":
+            program = SCRIPT_TEMPLATE.format(body="ctx = seamless.fromfile('{}')".format(file_path.as_posix()))
 
         else:
-            with open(file_path) as f:
-                file_contents = f.read()
+            with open(file_path) as fd:
+                file_contents = fd.read()
 
             program = SCRIPT_TEMPLATE.format(
-                body=SCRIPT_FILE_TEMPLATE.format(file_path=file_path, file_contents=file_contents)
+                body=SCRIPT_FILE_TEMPLATE.format(file_path=file_path.as_posix(), file_contents=file_contents)
             )
 
-    argv = [sys.argv[0], "-c", program, "-i"]
-    # TODO use tempfile
-    from IPython import start_ipython
-    sys.exit(start_ipython(argv))
+    with temp_file() as temp_file_path:
+        with open(temp_file_path, "w") as f:
+            f.write(program)
+
+        argv = [temp_file_path, "-i"]
+        from IPython import start_ipython
+        sys.exit(start_ipython(argv))
+
 
 
 if __name__ == "__main__":
