64cb95db124ceec718bb249832cfc6c9f1c84611
Angus Hollands <goosey15@gmail.com>
2017-08-16 11:36:21 +0100

    Do not add parse_silk or print_xml to tools -> move to wip Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='seamless-framework',
-    version='0.0.1',
+    version='0.1',
     packages=['seamless', 'seamless.gui', 'seamless.lib', 'seamless.lib.gui', 'seamless.lib.gui.gl', 'seamless.lib.ngl',
               'seamless.lib.plotly', 'seamless.core', 'seamless.core.pysynckernel', 'seamless.core.pythreadkernel',
               'seamless.silk', 'seamless.silk.classes', 'seamless.silk.registers', 'seamless.silk.transform',
@@ -14,8 +14,6 @@ setup(
     install_requires=['pandas', 'websockets', 'PyQt5', 'cson', 'numpy', 'PyOpenG', 'IPython'],
     author_email='',
     description='A framework to set up computations (and visualizations) that respond to changes in cells. Cells contain the input data as well as the source code of the computations, and all cells can be edited live.',
-    entry_points={'console_scripts': ['seamless=seamless.tools.seamless:main',
-                                      'parse_silk=seamless.tools.parse_silk:main',
-                                      'parse_xml=seamless.tools.parse_xml:main']
+    entry_points={'console_scripts': ['seamless=seamless.tools.seamless:main',]
                   }
 )
