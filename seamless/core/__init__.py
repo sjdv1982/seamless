a28cf44e90545bfbae3f2f5efa736c7b0d3b17a4
Sjoerd de Vries <sjdv1982@gmail.com>
2017-09-04 18:52:00 +0200

    added error property to cell; changed INVALID status flag to ERROR for consistency

--- seamless/core/__init__.py
+++ seamless/core/__init__.py
@@ -8,7 +8,7 @@ class SeamlessBase:
     _last_context = None
     name = None
 
-    StatusFlags = Enum('StatusFlags', ('OK', 'PENDING', 'INVALID', 'UNDEFINED', 'UNCONNECTED'))
+    StatusFlags = Enum('StatusFlags', ('OK', 'PENDING', 'UNDEFINED', 'UNCONNECTED', 'ERROR'))
     _status = StatusFlags.UNDEFINED
 
     def __init__(self):
