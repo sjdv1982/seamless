7ee56104756707b2cea5e9bd48b24d1df1b94470
Sjoerd de Vries <sjdv1982@gmail.com>
2017-09-05 18:36:48 +0200

    display numpy

--- wip/display-numpy/cell-display-numpy.py
+++ wip/display-numpy/cell-display-numpy.py
@@ -1,22 +1,28 @@
-from PyQt5.QtWidgets import QMainWindow, QLabel, QLayout, QHBoxLayout, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QLabel, QLayout, QHBoxLayout, QVBoxLayout, QWidget, QFrame
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtCore import Qt, QSize
 import numpy as np
+
+#partially based on code ported from https://gist.github.com/pavel-perina/1324ff064aedede0e01311aab315f83d
 
 class AspectLayout(QLayout):
     def __init__(self, aspect):
         self.aspect = aspect
         self.item = None
         super().__init__()
+        self.setContentsMargins(0,0,0,0)
+
     def addItem(self, item):
         assert self.item is None, "AspectLayout can contain only 1 widget"
         self.item = item
+
     def itemAt(self, index):
         if index != 0:
             return None
         if self.item is None:
             return None
         return self.item
+
     def takeAt(self, index):
         if index != 0:
             return None
@@ -25,22 +31,71 @@ class AspectLayout(QLayout):
         result = self.item
         self.item = None
         return result
+
+    def setGeometry(self, rect):
+        super().setGeometry(rect)
+        margins = self.getContentsMargins()
+        if self.item is not None:
+            availW = rect.width() - margins[1] - margins[3]
+            availH = rect.height() - margins[0] - margins[2]
+            h = availH
+            w = h * self.aspect
+            if w > availW:
+                x = margins[1]
+                w = availW
+                h = w/self.aspect
+                if self.item.alignment() &  Qt.AlignTop:
+                    y = margins[0]
+                elif self.item.alignment() &  Qt.AlignBottom:
+                    y = rect.height() - margins[2] - h
+                else:
+                    y = margins[0] + (availH-h) / 2
+            else:
+                y = margins[0]
+                if self.item.alignment() &  Qt.AlignLeft:
+                    x = margins[1]
+                elif self.item.alignment() &  Qt.AlignRight:
+                    x = rect.width() - margins[3] - w
+                else:
+                    x = margins[1] + (availW-w) / 2
+            self.item.widget().setGeometry(
+                rect.x() + x,
+                rect.y() + y,
+                w, h)
+
     def sizeHint(self):
+        margins = self.getContentsMargins()
         if self.item is None:
-            return QSize(0,0)
-        w,h = self.item.sizeHint()
-        return w, self.aspect * w
+            return QSize(margins[0]+margins[2],margins[1]+margins[3])
+        s = self.item.sizeHint()
+        w, h = s.width(), s.height()
+        return QSize(margins[0]+margins[2] + w, margins[1]+margins[3] + h)
+
+    def minimumSize(self):
+        margins = self.getContentsMargins()
+        if self.item is None:
+            return QSize(margins[0]+margins[2],margins[1]+margins[3])
+        s = self.item.minimumSize()
+        w, h = s.width(), s.height()
+        return QSize(margins[0]+margins[2] + w, margins[1]+margins[3] + h)
+
+    def expandingDirections(self):
+       return Qt.Horizontal | Qt.Vertical
+
     def hasHeightForWidth(self):
-        print("OK")
-        return True
+        return False
+
     def count(self):
         if self.item is None:
             return 0
         else:
             return 1
+
     def heightForWidth(self, width):
-        print("O", self.aspect, width)
-        return int(self.aspect * width)
+        margins = self.getContentsMargins()
+        height = (width - margins[1] - margins[3]) / self.aspect
+        height += margins[0] + margins[2]
+        return int(height)
 
 w = QWidget()
 asp = AspectLayout(1.0)
@@ -50,6 +105,7 @@ w.setWindowFlags(Qt.WindowStaysOnTopHint)
 l = QLabel()
 l.setScaledContents(True)
 asp.addWidget(l)
+l.setFrameStyle(QFrame.NoFrame)
 w.show()
 
 def update():
@@ -57,6 +113,8 @@ def update():
     arr = PINS.array.get()
     assert arr.dtype in (float, np.float32), arr.dtype
     arr = np.ascontiguousarray(arr)
+    if arr.ndim == 1:
+        arr = arr.reshape((len(arr), 1))
     if arr.ndim == 3:
         assert arr.shape[-1] == 3
         amin = arr.min(axis=0).min(axis=0)
@@ -65,9 +123,22 @@ def update():
         arr_norm = (arr - amin) / arange
         arr_norm_255 = ((arr_norm- 1e-6)*256).astype(np.uint8)
         width, height = arr.shape[1], arr.shape[0]
-        im = QImage(arr_norm_255, width, height, QImage.Format_RGB888)
+        im = QImage(arr_norm_255, width, height, width*3, QImage.Format_RGB888)
+    elif arr.ndim == 2:
+        amin = arr.min()
+        amax = arr.max()
+        arange = np.maximum(amax-amin, 1e-12)
+        arr_norm = (arr - amin) / arange
+        arr_norm_255 = ((arr_norm- 1e-6)*256).astype(np.uint8)
+        arr_color = np.zeros((arr.shape) + (3,), dtype=np.uint8)
+        arr_color[:,:,0] = arr_norm_255
+        arr_color[:,:,1] = 128 - np.abs(arr_norm_255.astype(int)-128)
+        arr_color[:,:,2] = 255 - arr_norm_255
+        width, height = arr_color.shape[1], arr_color.shape[0]
+        im = QImage(arr_color, width, height, width*3, QImage.Format_RGB888)
+
     pm = QPixmap.fromImage(im)
-    asp.aspect = height / width
+    asp.aspect = width / height
     w.updateGeometry()
     l.setPixmap(pm)
 def destroy():
--- wip/display-numpy/cell-gen-array.py
+++ wip/display-numpy/cell-gen-array.py
@@ -1,3 +1,8 @@
 import numpy as np
-a = np.random.random((100, 200, 3))
-return a
+from scipy.ndimage.filters import gaussian_filter
+sigma = 1
+np.random.seed(0)
+a = np.random.normal(size=(100, 200, 3))
+for n in range(3):
+    a[:,:,n] = gaussian_filter(a[:,:,n], sigma=sigma)
+return a[:,:,0]
