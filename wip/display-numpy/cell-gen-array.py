fb9bc944d7965697c8a982c858075e7c12c250b0
Sjoerd de Vries <sjdv1982@gmail.com>
2017-09-06 22:39:04 +0200

    array display works

--- wip/display-numpy/cell-gen-array.py
+++ wip/display-numpy/cell-gen-array.py
@@ -4,5 +4,12 @@ sigma = 1
 np.random.seed(0)
 a = np.random.normal(size=(100, 200, 3))
 for n in range(3):
-    a[:,:,n] = gaussian_filter(a[:,:,n], sigma=sigma)
-return a[:,:,0]
+    a[:,:,n] = gaussian_filter(a[:,:,n], sigma=sigma, mode="wrap")
+ret = np.zeros((100, 200))
+ret0 = a[:,:,0]
+ret[:50,:100] = ret0[50:,100:]
+ret[50:,:100] = ret0[:50,100:]
+ret[:50,100:] = ret0[50:,:100]
+ret[50:,100:] = ret0[:50,:100]
+return ret
+#return a[:,:,0]
