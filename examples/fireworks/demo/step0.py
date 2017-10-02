86638c65ff568a32d86b9ac992e36afe89e808dd
Sjoerd de Vries <sjdv1982@gmail.com>
2017-10-02 13:23:55 +0200

    snapshot

---
+++ examples/fireworks/demo/step0.py
@@ -0,0 +1,6 @@
+from seamless import cell, pythoncell, context, reactor, transformer
+from seamless.lib.gui.basic_editor import edit
+from seamless.lib.gui.basic_display import display
+from seamless.lib import link
+
+ctx = context()
