c6889d2df99f40bb474b292e846c7246ab0fb565
Sjoerd de Vries <sjdv1982@gmail.com>
2017-10-07 17:14:05 +0200

    fireworks tutorial finished, only need demo part

--- examples/fireworks/tutorial/step1.py
+++ examples/fireworks/tutorial/step1.py
@@ -1,11 +1,9 @@
-from seamless.lib.gui.gl import glprogram
-
-ctx.vertexdata = cell("array")
-
 # Generate three-dimensional points (even though we will ignore Z)
 N = 10000
 import numpy as np
 values = np.random.normal(0.0, 0.2, (N, 3)).astype(np.float32)
+
+ctx.vertexdata = cell("array")
 ctx.vertexdata.set(values)
 
 #add a bar in the lower right corner
@@ -15,12 +13,12 @@ values = np.concatenate((values, bar)).astype(np.float32)
 ctx.vertexdata.set(values)
 
 # Discretize manually
-im = np.zeros((480,640)) #in an image, the first dim is Y!
+im = np.zeros((640,480))
 values_x = (values[:,0]*320+320).astype(int)
 values_x = np.minimum(values_x, 640)
 values_y = (values[:,1]*240+240).astype(int)
 values_y = np.minimum(values_y, 480)
-im[values_y, values_x] = 1
-im[::-1,:] = im #in an image, Y goes from top to bottom!
+im[values_x, values_y] = 1
+im = np.rot90(im)
 
 ctx.im = cell("array").set(im)
