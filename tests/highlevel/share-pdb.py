8ee75181d1006127f9e6b8429d6b77f69bf64eb8
Sjoerd de Vries <sjdv1982@gmail.com>
2019-04-25 19:48:04 +0200

    pdb sharing, tweaked

--- tests/highlevel/share-pdb.py
+++ tests/highlevel/share-pdb.py
@@ -8,15 +8,15 @@ ctx.pdb0.share()
 
 ctx.filter_pdb = Transformer()
 ctx.filter_pdb.language = "bash"
-ctx.filter_pdb.code = "head pdb0"
+ctx.filter_pdb.code = "grep ATOM pdb0 | head -50"
 ctx.filter_pdb.pdb0 = ctx.pdb0
 
 ctx.pdb = ctx.filter_pdb
 ctx.pdb.celltype = "text"
 ctx.pdb.share()
 
-ctx.equilibrate()
+ctx.code >> ctx.filter_pdb.code
+ctx.code.share()
 
-print(ctx.pdb.value)
-print(ctx.status)
-print(ctx.filter_pdb.result.value)
+ctx.code.mount("/tmp/code.bash")
+ctx.equilibrate()
