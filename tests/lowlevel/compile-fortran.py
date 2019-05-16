245ff1aaa2655b6e97bf5e71323fbc22d821b9c5
Sjoerd de Vries <sjdv1982@gmail.com>
2019-05-16 16:06:23 +0200

    Fortran90 and mixed-language compiled modules are working

---
+++ tests/lowlevel/compile-fortran.py
@@ -0,0 +1,65 @@
+from seamless.core.build_module import build_module
+
+######################################################################
+# 1: set up compiled module
+######################################################################
+
+code = """
+function add(a, b) result(r) bind(C)
+    use iso_c_binding
+    implicit none
+    integer(c_int), VALUE:: a, b
+    real(c_float) r
+    r = a + b
+end function
+"""
+module = {
+    #"target": "debug",
+    "type": "compiled",
+    "objects": {
+        "main": {
+            "code": code,
+            "language": "f90",
+        },
+    },
+    "public_header": {
+        "language": "c",
+        "code": "float add2(int a, int b);"
+    }
+}
+
+######################################################################
+# 2: compile it to binary module
+######################################################################
+
+testmodule = build_module(module)[1].lib
+print(testmodule.add(2,3))
+
+######################################################################
+# 3: test it in a context
+######################################################################
+
+from seamless.core import context, cell, transformer, macro_mode_on
+with macro_mode_on():
+    ctx = context(toplevel=True)
+    ctx.module = cell("plain")
+    ctx.module.set(module)
+    tf = ctx.tf = transformer({
+        "a": ("input", "ref", "plain"),
+        "b": ("input", "ref", "plain"),
+        "testmodule": ("input", "module"),
+        "result": ("output", "ref", "plain"),
+    })
+    ctx.module.connect(tf.testmodule)
+    tf.a.cell().set(12)
+    tf.b.cell().set(13)
+    tf.code.cell().set("""
+from .testmodule import lib
+print("ADD", lib.add(a,b))
+result = testmodule.lib.add(a,b)
+    """)
+    ctx.result = cell("plain")
+    ctx.tf.result.connect(ctx.result)
+
+ctx.equilibrate()
+print(ctx.result.value)
---
+++ tests/lowlevel/compile-mixedlang.py
@@ -0,0 +1,81 @@
+from seamless.core.build_module import build_module
+
+######################################################################
+# 1: set up compiled module
+######################################################################
+
+code_fortran = """
+function add(a, b) result(r) bind(C)
+    use iso_c_binding
+    implicit none
+    integer(c_int), VALUE:: a, b
+    real(c_float) r
+    r = a + b
+end function
+"""
+code_cpp = """
+extern "C" float add(int a, int b);
+extern "C" float add2(int a, int b) {
+    return 2 * add(a, b);
+}
+"""
+
+public_header = """
+float add(int a, int b);
+float add2(int a, int b);
+"""
+
+module = {
+    "type": "compiled",
+    "objects": {
+        "obj1": {
+            "code": code_fortran,
+            "language": "f90",
+        },
+        "obj2": {
+            "code": code_cpp,
+            "language": "cpp",
+        },
+    },
+    "public_header": {
+        "language": "c",
+        "code": public_header, 
+    }
+}
+
+######################################################################
+# 2: compile it to binary module
+######################################################################
+
+testmodule = build_module(module)[1].lib
+print(testmodule.add(2,3))
+print(testmodule.add2(2,3))
+
+######################################################################
+# 3: test it in a context
+######################################################################
+
+from seamless.core import context, cell, transformer, macro_mode_on
+with macro_mode_on():
+    ctx = context(toplevel=True)
+    ctx.module = cell("plain")
+    ctx.module.set(module)
+    tf = ctx.tf = transformer({
+        "a": ("input", "ref", "plain"),
+        "b": ("input", "ref", "plain"),
+        "testmodule": ("input", "module"),
+        "result": ("output", "ref", "plain"),
+    })
+    ctx.module.connect(tf.testmodule)
+    tf.a.cell().set(12)
+    tf.b.cell().set(13)
+    tf.code.cell().set("""
+from .testmodule import lib
+print("ADD2", lib.add2(a,b))
+result = testmodule.lib.add2(a,b)
+    """)
+    ctx.result = cell("plain")
+    ctx.tf.result.connect(ctx.result)
+
+ctx.equilibrate()
+print(ctx.result.value)
