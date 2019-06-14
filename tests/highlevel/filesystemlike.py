27223a3ce278b5b01f8f5c6e06ac9ac3d4c09618
Sjoerd de Vries <sjdv1982@gmail.com>
2019-06-14 15:27:17 +0200

    filesystem-like test, with pseudo-cyclic dependency

---
+++ tests/highlevel/filesystemlike.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from seamless.highlevel import Context
+ctx = Context()
+ctx.filesystem = {"test.txt": """
+This
+is
+a
+test
+"""
+}
+ctx.job = lambda input: None
+ctx.job.language = "bash"
+ctx.job.code = "awk 'NF > 0{print $0 \" OK!\"}' input"
+ctx.job.input = ctx.filesystem["test.txt"]
+ctx.result = ctx.job
+ctx.equilibrate()
+print(ctx.result.value)
+print("START")
+ctx.filesystem["result.txt"] = ctx.result
+ctx.equilibrate(1)
+print(ctx.result.value)
+print(ctx.filesystem.value)
