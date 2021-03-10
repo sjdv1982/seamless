4669343f6f37df2814e3d10ee96bd63f9df4fee2
Sjoerd de Vries <sjdv1982@gmail.com>
2021-03-10 16:39:33 +0100

    longer bash/docker test that included mixed-plain output

--- tests/highlevel/bash.py
+++ tests/highlevel/bash.py
@@ -3,7 +3,7 @@ from seamless.highlevel import Context
 ctx = Context()
 ctx.code = "head -$lines testdata > RESULT"
 ctx.code.celltype = "text"
-ctx.code.mount("/tmp/test.bash")
+ctx.code.mount("/tmp/test.bash", authority="cell")
 ctx.tf = lambda lines, testdata: None
 ctx.tf.language = "bash"
 ctx.tf.testdata = "a \nb \nc \nd \ne \nf \n"
@@ -15,10 +15,25 @@ ctx.result.mount("/tmp/result", "w")
 ctx.translate(force=True)
 ctx.compute()
 print(ctx.result.value)
-ctx.code = "head -3 testdata > firstdata; tar czf RESULT testdata firstdata"
+ctx.code = "head -3 testdata > firstdata; tar hczf RESULT testdata firstdata"
 ctx.compute()
 print(ctx.result.value)
 ctx.code = "python3 -c 'import numpy as np; np.save(\"test\",np.arange(12)*3)'; cat test.npy > RESULT"
 ctx.compute()
 print(ctx.tf.result.value)
+print(ctx.tf.status)
+ctx.code = """
+python3 -c 'import numpy as np; np.save(\"test\",np.arange(12)*3)'
+echo 'hello' > test.txt
+tar hczf RESULT test.npy test.txt
+"""
+ctx.result = ctx.tf
+ctx.result.celltype = "structured"
+ctx.result_npy = ctx.result["test.npy"]
+ctx.result_txt = ctx.result["test.txt"]
+ctx.compute()
+print("")
+print(ctx.result.value)
+print(ctx.result_npy.value)
+print(ctx.result_txt.value)
 print(ctx.tf.status)
