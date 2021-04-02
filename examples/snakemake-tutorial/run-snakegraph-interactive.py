e5e48571625af154e7de6e53a978a15a98bd04a4
Sjoerd de Vries <sjdv1982@gmail.com>
2021-04-02 16:09:10 +0200

    minor

--- examples/snakemake-tutorial/run-snakegraph-interactive.py
+++ examples/snakemake-tutorial/run-snakegraph-interactive.py
@@ -113,7 +113,7 @@ def bind(file, mode):
 
 def list_files():
     print("File system contents:")
-    for fs_cellname in ctx.fs.children("cell"):
+    for fs_cellname in ctx.fs.get_children("cell"):
         fs_cell = getattr(ctx.fs, fs_cellname)
         value = fs_cell.value
         value2 = RichValue(value, need_form=True)
