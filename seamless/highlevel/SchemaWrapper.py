b7aaedad7780cfcd50ccbd4fb24ab720feab4462
Sjoerd de Vries <sjdv1982@gmail.com>
2021-04-07 22:26:43 +0200

    fix https://github.com/sjdv1982/seamless/issues/27

--- seamless/highlevel/SchemaWrapper.py
+++ seamless/highlevel/SchemaWrapper.py
@@ -3,7 +3,7 @@ from weakref import ref
 class SchemaWrapper:
     def __init__(self, parent, wrapped, path):
         self._wrapped = wrapped
-        assert path in ("SCHEMA", "RESULTSCHEMA")        
+        assert path in ("SCHEMA", "RESULTSCHEMA")
         self._path = path
         self._parent = ref(parent)
 
@@ -13,6 +13,18 @@ class SchemaWrapper:
     @property
     def _virtual_path(self):
         return self._parent()._path + (self._path,)
+
+    def add_validator(self, validator, name):
+        """Adds a validator function (in Python) to the schema.
+
+        The validator must take a single argument, the (buffered) value of the cell
+        It is expected to raise an exception (e.g. an AssertionError)
+        if the value is invalid.
+
+        If a previous validator with the same name exists,
+        that validator is overwritten.
+        """
+        return self._parent().handle.add_validator(validator, name=name)
 
     def __getattr__(self, attr):
         if attr == "_wrapped":
@@ -40,4 +52,3 @@ class SchemaWrapper:
 
     def __repr__(self):
         return repr(self._wrapped)
-
--- tests/highlevel/schema.py
+++ tests/highlevel/schema.py
@@ -28,10 +28,58 @@ ctx.a.y = 88
 def validate(self):
     print("VALIDATE", self)
     assert self.x > 0
-ctx.a.add_validator(validate, "validate")
+ctx.a.schema.add_validator(validate, "validate")
 ctx.a.x = -1
 ctx.translate()
-print("OK")
-print(ctx.a._get_cell().value)
-print(ctx.a._get_cell().handle)
+print(ctx.a.value)
+print(ctx.a.buffered)
+print(ctx.a.handle)
 ctx.compute()
+print(ctx.a.value)
+print(ctx.a.buffered)
+print(ctx.a.handle)
+ctx.a.x = 2
+ctx.a.handle.xx = property(lambda self: self.x + 10)
+ctx.compute()
+print(ctx.a.value)
+print(ctx.a.value.x)
+print(ctx.a.value.xx)
+print()
+
+ctx.a.z = 100
+def setter(self, value):
+    self.z = value
+a = ctx.a.handle # this will synchronize with ctx.a.value after computation
+a.zz = property(lambda self: self.z, setter)
+print(a.zz)
+a.zz = 200
+print(a.z)
+ctx.compute()
+print(ctx.a.value.z, ctx.a.value.zz)
+print()
+
+ctx.a.q = 101
+def setter(self, value):
+    self.q = value
+ctx.a.handle.qq = property(lambda self: self.q, setter)
+ctx.a.handle.get_qx = lambda self: self.q + self.x
+ctx.compute()
+a = ctx.a.value
+print(a.q, a.qq, a.get_qx())  # this works, because ctx.a.value gets the full schema
+a.z = -a.z # will not update ctx.a.value, because a deep copy is created every time
+a.qq = 201 # will not update ctx.a.value, because a deep copy is created every time
+ctx.compute()
+print(a.z, ctx.a.value.z)
+print(a.qq, ctx.a.value.qq)
+print()
+
+ctx.b = ctx.a.b
+a = ctx.a.handle
+a.b = {}
+a.b.c = 12
+def setter(self, value):
+    self.d = value
+a.b.set_d = setter
+a.b.set_d(80)
+ctx.compute()
+print(ctx.b.value)
