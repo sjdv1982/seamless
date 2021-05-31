29ed3803d3c576e449b66b67920cf44fd2d4d321
Sjoerd de Vries <sjdv1982@gmail.com>
2021-05-31 11:37:11 +0200

    https://github.com/sjdv1982/seamless/issues/84 : added environment checks for image (except version and checksum) and for conda. Seamless commands not set DOCKER_IMAGE to rpbs/seamless

--- seamless/core/environment.py
+++ seamless/core/environment.py
@@ -1,36 +1,153 @@
+from re import I
+import re
 import conda
 from conda.models.match_spec import MatchSpec
 from conda.cli.python_api import Commands, run_command as conda_run
+import os, stat
+
+from numpy import isin
+
+DOCKER_SOCKET = "/var/run/docker.sock"
+DOCKER_IMAGE = os.environ.get("DOCKER_IMAGE", "rpbs/seamless")
+print(DOCKER_IMAGE)
+
+def check_docker_power():
+    if not os.path.exists(DOCKER_SOCKET):
+        return False    
+    mode = os.stat(DOCKER_SOCKET).st_mode
+    if stat.S_ISSOCK(mode):
+        return os.access(DOCKER_SOCKET, os.R_OK | os.W_OK)  
+
+def check_conda_power():
+    return False  # Seamless has no support for creating conda environments on-the-fly
+
+def check_ipython_power():
+    return True
+
+"""
+validate_XXX return a tuple
+- Element 0:
+    - True: match
+    - None: no match
+    - False: invalid attribute
+- Element 1: error message
+"""
+
+async def validate_image(environment):
+    image = environment.get("image")
+    if image is None:
+        return None, None
+    err0 = "Malformed environment.image attribute"
+    if not isinstance(image, dict):
+        return False, err0
+    if "name" not in image:
+        return False, err0
+    if "version" in image or "checksum" in image:
+        return None, "Not implemented: version or checksum"
+    if image["name"] == DOCKER_IMAGE:
+        return True, None
+    else:
+        err = "Cannot execute code locally: current image is '{}', whereas '{}' is required"
+        return None, err.format(DOCKER_IMAGE, image["name"])
+
+async def validate_capabilities(environment):
+    capabilities = environment.get("capabilities")
+    if capabilities is None:
+        return None, None
+    return None, "Not implemented"
+
 
 async def validate_conda_environment(environment):
     condenv = environment.get("conda")
     if condenv is None:
-        return False
-    for dep in condenv.get("dependencies", []):
-        ms = MatchSpec(dep)
-        info = conda_run(Commands.LIST, ["-f", ms.name])[0]
-        for l in info.split("\n"):
-            l = l.strip()
-            if l.startswith("#"):
-                continue
-            ll = l.split()
-            if len(ll) < 2:
-                continue
-            if ll[0] != ms.name:
-                continue
-            if ms.version.match(ll[1]):                
-                break
+        return None, None
+    err0 = "Malformed environment.conda attribute"
+    if not isinstance(condenv, dict):
+        return False, err0
+    err = ""
+    try:
+        for dep in condenv.get("dependencies", []):
+            ms = MatchSpec(dep)
+            info = conda_run(Commands.LIST, ["-f", ms.name])[0]
+            for l in info.split("\n"):
+                l = l.strip()
+                if l.startswith("#"):
+                    continue
+                ll = l.split()
+                if len(ll) < 2:
+                    continue
+                if ll[0] != ms.name:
+                    continue
+                if ms.version.match(ll[1]):                
+                    break
+                else:
+                    msg = "Conda package '{}': {} installed, but {} required"
+                    err += msg.format(ms.name, ll[1], ms.version) + "\n"
+                    break
             else:
-                # TODO: record incompatible version
-                pass
-        else:
-            return False
-    return True
+                err += "Conda package '{}' not installed\n".format(ms.name)
+    except (KeyError, ValueError, AttributeError):
+        return False, err0
+    if len(err):
+        return None, err.rstrip("\n")
+    else:
+        return True, None
         
     
 
+power_checkers = {
+    "ipython": check_ipython_power,
+    "conda": check_conda_power,
+    "image": check_docker_power,
+}
+
 async def validate_environment(environment):
-    if await validate_conda_environment(environment):
-        return
-    ### TODO
-    raise ValueError("Incompatible environment")  # TODO: error message
+    if not isinstance(environment, dict):
+        raise TypeError("Malformed environment")
+    result_conda = await validate_conda_environment(environment)
+    result_capabilities = await validate_capabilities(environment)
+    result_image = await validate_image(environment)
+    powers = environment.get("powers", [])
+    
+    for power in powers:
+        if power not in power_checkers:
+            raise ValueError("Unknown environment power {}".format(power))
+        has_power = power_checkers[power]()
+        if not has_power:
+            raise ValueError("Environment power cannot be granted: '{}'".format(power))
+
+    err = ""
+    if result_capabilities[0] == False:
+        err += "Capabilities:\n  " + str(result_capabilities[1]) + "\n"    
+    if result_image[0] == False:
+        err += "Image:\n  " + str(result_image[1]) + "\n"
+    if result_conda[0] == False:
+        err += "Conda:\n  " + str(result_conda[1]) + "\n"
+    if len(err):
+        raise ValueError("Environment error:\n" + err)
+
+    err = ""
+    if result_capabilities[0] in (None, True):
+        if "capabilities" in powers:
+            return
+        if result_capabilities[0] == True:
+            return
+        if result_capabilities[1] is not None:
+            err += "Capabilities:\n  " + str(result_capabilities[1]) + "\n"
+    if result_image[0] in (None, True):
+        if "image" in powers:
+            return
+        if result_image[0] == True:
+            return
+        if result_image[1] is not None:
+            err += "Image:\n  " + str(result_image[1]) + "\n"
+    if result_conda[0] in (None, True):
+        if "conda" in powers:
+            return
+        if result_conda[0] == True:
+            return
+        if result_conda[1] is not None:
+            err += "Conda:\n  " + str(result_conda[1]) + "\n"
+    if not len(err):
+        err = "Unknown environment error"
+    raise ValueError("Environment error:\n" + err)
