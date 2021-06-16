656464272e179da4dd1fd2c4df1162484d40dc0e
Sjoerd de Vries <sjdv1982@gmail.com>
2021-06-16 17:14:11 +0200

    added environment examples (Rust and Go)

--- tests/highlevel/environment.py
+++ tests/highlevel/environment.py
@@ -1,17 +1,25 @@
+"""
+Simple examples on how to modify the Seamless environment
+- Add an example conda requirement
+- Add Rust support via rustc
+"""
+
 import traceback
 
 from seamless.highlevel import Context, Cell, Transformer
 ctx = Context()
+env = ctx.environment
 
+# Define an environment that requires Python
 conda_environment ="""
 dependencies:
    - python
 """
-env = ctx.environment
 env.set_conda(conda_environment, "yaml")
-ctx.translate()
+ctx.translate()  # success
 
-conda_environment ="""
+# Define an environment that requires Python 10 or higher (fail)
+conda_environment = """
 dependencies:
    - python>=10
 """
@@ -20,11 +28,17 @@ env.set_conda(conda_environment, "yaml")
 try:
     ctx.translate()
 except ValueError as exc:
-    print(traceback.format_exception_only(type(exc), exc)[0])
+    traceback.print_exc(limit=0)
+    print()
 
+# Reset the environment
 env.set_conda(None, "yaml")
 ctx.translate()
 
+
+# Define a transformer with signature 
+#    (a: int, b: int) -> int
+#    and a=13, b=16
 ctx.tf = Transformer()
 ctx.tf.a = 13
 ctx.tf.b = 16
@@ -37,52 +51,106 @@ print(ctx.tf.schema)
 print(ctx.tf.result.schema)
 print(ctx.tf.a.value)
 
+# Set the language as Rust
+#   => fail, unknown language
 try:
-    ctx.tf.language = "go"
+    ctx.tf.language = "rust"
 except KeyError as exc:
-    print(traceback.format_exception_only(type(exc), exc)[0])
+    traceback.print_exc(limit=0)
+    print()
 
+# Have a look how languages are defined...
+languages = env.get_languages("cson")
+print("\n".join(languages.splitlines()[:10]))
+print()
+
+# Create a new language "rust"
 languages = env.get_languages("plain")
-languages["go"] = {
-    "extension": "go",
+languages["rust"] = {
+    "extension": "rs",
     "mode": "compiled",
 }
 env.set_languages(languages, "plain")
 
-ctx.tf.language = "go"  # will reset all inputs!
-ctx.tf.code = """
-package main
+# Set the language as rust => success
+ctx.tf.language = "rust"
 
-func transformer(a int, b int) int {
-    return a + b
+# Generate C header
+ctx.compute()
+print(ctx.tf.header.value)
+print()
+
+# Write some Rust code
+# Unlike Go, Rust cannot import C header declarations
+# So we must take care ourselves that the function signature
+#  of transform() is correct
+ctx.tf.code = """
+#[no_mangle] pub unsafe extern "C"
+fn transform (a: i32, b: i32,  c: *mut i32) -> i32
+{
+    (*c) = a + b;
+    return 0;
 }
 """
 ctx.compute()
-LDFLAGS = -lgo -lgobegin
+print(ctx.tf.exception)  # failure: no Rust compiler
+print()
 
-ctx.tf.a = 13
-ctx.tf.b = 16
-ctx.compute()
-print(ctx.tf.exception)
+# Have a look how compilers are defined...
+compilers = env.get_compilers("cson")
+print("\n".join(compilers.splitlines()[:20]))
+print()
 
+# Set up "rustc" as the Rust compiler
+# rustc will produce one archive (.a file)
+#  for each crate (.rs file).
+# Therefore, the compiler mode is "archive"
 languages = env.get_languages("plain")
-languages["go"] = {
-    "extension": "go",
+languages["rust"] = {
+    "extension": "rs",
     "mode": "compiled",
-    "compiler": "gccgo"
+    "compiler": "rustc"
 }
 env.set_languages(languages, "plain")
 compilers = env.get_compilers("plain")
-compilers["gccgo"] = compilers["gcc"]
+compilers["rustc"] = {
+    "mode": "archive", 
+    "options": ["--crate-type=staticlib"],
+    "debug_options": ["--crate-type=staticlib"],
+    "profile_options": [],
+    "release_options": [],
+    "compile_flag": "",
+    "output_flag": "-o",
+}
 env.set_compilers(compilers, "plain")
 ctx.translate()
 ctx.tf.clear_exception()
 ctx.compute()
 
+# If rustc is installed, there will be no exception
+# else, the error message will be a bit long..
 print(ctx.tf.exception)
+ctx.tf.clear_exception()
 
-env.set_which(["gccgo"], "plain")
+# This will give a much nicer error is rustc is not there
+#  as Seamless will now refuse to translate
+env.set_which(["rustc"], "plain")
+try:
+    ctx.translate()
+except ValueError:
+    traceback.print_exc(limit=0)
+    exit(0)
+
+# If we get here, then everything should be fine
+print(ctx.tf.exception)  # None
+print(ctx.tf.result.value) # 29
+
+ctx.tf.a = 18
 ctx.compute()
+print(ctx.tf.result.value) # 34
 
-
-# TODO: link options
+# We can also provide linker options (not needed here)
+ctx.tf.link_options = ["-lm"]  # no effect
+ctx.compute()
+print(ctx.tf.exception) # None
+print(ctx.tf.result.value) # 34
---
+++ tests/highlevel/environment2.py
@@ -0,0 +1,159 @@
+"""Another example on how to modify the Seamless environment,
+adding Go support via "go build"
+"""
+
+import traceback
+
+from seamless.highlevel import Context, Cell, Transformer
+ctx = Context()
+env = ctx.environment
+
+# Define a transformer with signature 
+#    (a: int, b: int) -> int
+#    and a=13, b=16
+ctx.tf = Transformer()
+ctx.tf.a = 13
+ctx.tf.b = 16
+ctx.compute()
+ctx.tf.example.a = 0
+ctx.tf.example.b = 0
+ctx.tf.result.example = 0
+
+print(ctx.tf.schema)
+print(ctx.tf.result.schema)
+print(ctx.tf.a.value)
+
+# Set the language as Go
+#   => fail, unknown language
+try:
+    ctx.tf.language = "go"
+except KeyError as exc:
+    traceback.print_exc(limit=0)
+    print()
+
+# Have a look how languages are defined...
+languages = env.get_languages("cson")
+print("\n".join(languages.splitlines()[:10]))
+print()
+
+# Create a new language "go"
+languages = env.get_languages("plain")
+languages["go"] = {
+    "extension": "go",
+    "mode": "compiled",
+}
+env.set_languages(languages, "plain")
+
+# Set the language as Go => success
+ctx.tf.language = "go"
+
+# Generate C header
+ctx.compute()
+print(ctx.tf.header.value)
+print()
+
+# Write some Go code
+# Go can directly use C header declarations
+# Here, we make deliberately a mistake, and the compiler will catch it
+ctx.tf.code = """
+package main
+
+/*
+int transform(int a, int b, int *result); //copied from ctx.tf.header.value
+*/
+import "C"
+
+//export transform
+func transform(a C.int, b C.int) C.int {  //wrong signature! will give compiler error
+    return a + b + 2000
+}
+func main(){}
+"""
+ctx.compute()
+
+# ... but first, we get a complaint that there is no Go compiler
+print(ctx.tf.exception)
+print()
+
+# Have a look how compilers are defined...
+compilers = env.get_compilers("cson")
+print("\n".join(compilers.splitlines()[:20]))
+print()
+
+# Set up "go build" as the Go compiler
+# "go build" will produce a single archive (.a file)
+#  for the entire package (all .go files).
+# Therefore, the compiler mode is "package"
+
+languages = env.get_languages("plain")
+languages["go"] = {
+    "extension": "go",
+    "mode": "compiled",
+    "compiler": "go build"
+}
+env.set_languages(languages, "plain")
+compilers = env.get_compilers("plain")
+compilers["go build"] = {
+    "mode": "package",
+    "options": ["-buildmode=c-archive"],
+    "debug_options": ["-buildmode=c-archive", '-gcflags "all=-N -l"'],
+    "profile_options": [],
+    "public_options": [],
+    "compile_flag": "",
+    "output_flag": "-o",
+
+}
+env.set_compilers(compilers, "plain")
+ctx.translate()
+ctx.tf.clear_exception()
+ctx.compute()
+
+# Now we get either:
+# 1. The compiler error mentioned above (signature mismatch)
+# or:
+# 2. A complaint that 'go' is not available
+print(ctx.tf.exception)
+ctx.tf.clear_exception()
+print()
+
+# This will give a much nicer error for case 2.
+#  as Seamless will now refuse to translate
+env.set_which(["go"], "plain")
+try:
+    ctx.translate()
+except ValueError:
+    traceback.print_exc(limit=0)
+    exit(0)
+
+# If we get here, then there is a working compiler
+#   all we need to do is fix the function signature
+ctx.tf.code = """
+package main
+
+/*
+int transform(int a, int b, int *result); //copied from ctx.tf.header.value
+*/
+import "C"
+
+//export transform
+func transform(a C.int, b C.int, result *C.int) C.int {  //correct
+    *result = a + b + 2000
+    return 0
+}
+func main(){}
+"""
+ctx.compute()
+print(ctx.tf.exception) # None
+print(ctx.tf.result.value) # 2029
+
+ctx.tf.a = 80
+ctx.compute()
+print(ctx.tf.result.value) # 2096
+
+# We can even launch debugging 
+#  GDB is not very good, but other debuggers (Delve) exist
+# TODO: source file mapping (Seamless only does this for gcc compilers)
+ctx.tf.debug = True
+ctx.tf.a = 18
+ctx.compute()
+print(ctx.tf.result.value)
