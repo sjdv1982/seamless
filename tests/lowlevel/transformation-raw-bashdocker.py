e445d3c2a8a9f69c4bf916c91c6f72b56bbc9c07
Sjoerd de Vries <sjdv1982@gmail.com>
2023-09-14 12:07:08 +0200

    fixed raw bashdocker test, add delegation option

--- tests/lowlevel/transformation-raw-bashdocker.py
+++ tests/lowlevel/transformation-raw-bashdocker.py
@@ -1,20 +1,25 @@
-import inspect, textwrap
+import os
 
 import seamless
-seamless.delegate(False)
+
+if "DELEGATE" in os.environ:
+    delegation = True
+    seamless.config.delegate()
+else:
+    delegation = False
+    seamless.config.delegate(False)
 
 from seamless import calculate_checksum
 from seamless.core.cache.buffer_cache import buffer_cache
 from seamless.core.cache.transformation_cache import (
-    transformation_cache, DummyTransformer, tf_get_buffer, 
-    syntactic_is_semantic, syntactic_to_semantic, 
+    transformation_cache, tf_get_buffer, 
     transformation_cache
 )
 
 from seamless.core.protocol.serialize import serialize
 
 async def build_transformation():
-    bash_code = "nginx -v"
+    bash_code = "nginx -v >& RESULT"
     inp = {
         "code": ("text", bash_code),
         "__env__": ("plain", {"docker": {"name": "nginx:1.25.2"}}),
@@ -38,10 +43,11 @@ async def build_transformation():
     tf_checksum = calculate_checksum(tf_buf)
     buffer_cache.cache_buffer(tf_checksum, tf_buf)
     
-    tf = DummyTransformer(tf_checksum)
-    result = await transformation_cache.run_transformation_async(tf_checksum, tf_dunder=tf_dunder, fingertip=False)
-    if result is not None:
-        result = buffer_cache.get_buffer(result, remote=False)
+    result_checksum = await transformation_cache.run_transformation_async(tf_checksum, tf_dunder=tf_dunder, fingertip=False)
+    if result_checksum is not None:
+        result = buffer_cache.get_buffer(result_checksum, remote=(delegation==True))
+        if delegation:
+            transformation_cache.undo(tf_checksum)            
     if result is not None:
         try:
             result = result.decode()
