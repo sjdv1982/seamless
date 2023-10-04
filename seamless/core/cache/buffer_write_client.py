3ce365f66d6f7effe953c8a316203d6780334b13
Sjoerd de Vries <sjdv1982@gmail.com>
2023-10-05 00:14:11 +0200

    fix closed connection during buffer write

--- seamless/core/cache/buffer_write_client.py
+++ seamless/core/cache/buffer_write_client.py
@@ -1,4 +1,4 @@
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, ChunkedEncodingError
 
 from seamless.util import parse_checksum
 
@@ -6,10 +6,16 @@ def has(session, url, checksum):
     checksum = parse_checksum(checksum)
     assert checksum is not None
     path = url + "/has"
-    with session.get(path, json=[checksum]) as response:
-        if int(response.status_code/100) in (4,5):
-            raise ConnectionError()
-        result = response.json()
+    for trial in range(10):
+        try:
+            with session.get(path, json=[checksum]) as response:
+                if int(response.status_code/100) in (4,5):
+                    raise ConnectionError()
+                result = response.json()
+        except ChunkedEncodingError:
+             continue
+        break
+
     if not isinstance(result, list) or len(result) != 1:
             raise ValueError(result)
     if not isinstance(result[0], bool):
