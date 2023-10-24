d9e354f75e60821b2d7dd51299c424024067f80e
Sjoerd de Vries <sjdv1982@gmail.com>
2023-10-24 09:38:49 +0200

    assistant client guard against disconnect

--- seamless/assistant_client.py
+++ seamless/assistant_client.py
@@ -2,6 +2,7 @@ import asyncio
 import aiohttp
 import os
 #session = aiohttp.ClientSession()
+from aiohttp.client_exceptions import ServerDisconnectedError
     
 async def run_job(checksum, tf_dunder):
     from seamless.highlevel import Checksum
@@ -23,19 +24,24 @@ async def run_job(checksum, tf_dunder):
     # One session per request is really bad... but what can we do?
     async with aiohttp.ClientSession() as session:
         data={"checksum":checksum, "dunder":tf_dunder}
-        while 1:
-            async with session.put(assistant, json=data,timeout=timeout) as response:
-                content = await response.read()
-                if response.status == 202:
-                    await asyncio.sleep(0.1)
-                    continue
-                try:
-                    content = content.decode()
-                except UnicodeDecodeError:
-                    pass                
-                if response.status != 200:
-                    raise RuntimeError(f"Error {response.status} from assistant:" + content)
+        for retry in range(5):
+            try:
+                while 1:
+                    async with session.put(assistant, json=data,timeout=timeout) as response:
+                        content = await response.read()
+                        if response.status == 202:
+                            await asyncio.sleep(0.1)
+                            continue
+                        try:
+                            content = content.decode()
+                        except UnicodeDecodeError:
+                            pass                
+                        if response.status != 200:
+                            raise RuntimeError(f"Error {response.status} from assistant:" + content)
+                        break
                 break
+            except ServerDisconnectedError:
+                continue
 
     result_checksum = parse_checksum(content)
     return result_checksum
