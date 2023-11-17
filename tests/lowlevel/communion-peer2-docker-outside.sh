a8da3978ec37bfa19d41b5104cd685df2046c87c
Sjoerd de Vries <sjdv1982@gmail.com>
2023-11-17 10:48:06 +0100

    clean up non-automatic low-level tests

--- tests/lowlevel/communion-peer2-docker-outside.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export SEAMLESS_COMMUNION_ID=Peer2
-port=$(docker port Peer1 | grep 8601 | sed 's/:/ /' | awk '{print $4}')
-export SEAMLESS_COMMUNION_INCOMING=localhost:$port
-python3 communion-peer2.py
