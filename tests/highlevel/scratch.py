d69e12f65c3156f6af0eb14224a9a992ebe217db
Sjoerd de Vries <sjdv1982@gmail.com>
2023-11-22 15:48:10 +0100

    add high-level scratch test

--- tests/highlevel/scratch.py
+++ tests/highlevel/scratch.py
@@ -1,15 +1,23 @@
-from seamless.highlevel import Context
+import seamless
+seamless.delegate(level=3)
+
+from seamless.highlevel import Cell, Context
 ctx = Context()
-ctx.a = {"x": 123}
-z = ctx.get_zip()
-print("ZIP size:", len(z))
-ctx.b = ctx.a.x
+ctx.a = Cell("int").set(2)
+ctx.b = Cell("int").set(3)
+ctx.tf = lambda a,b: a+b
+ctx.tf.scratch = True
+ctx.tf.a = ctx.a
+ctx.tf.b = ctx.b
+ctx.result = ctx.tf
+ctx.result.scratch = True
 ctx.compute()
-print(ctx.b.value, ctx.b.checksum)
-z = ctx.get_zip()
-print("ZIP size:", len(z))
-ctx.b.scratch = True
+print(ctx.result.value)
+print(ctx.result.checksum)
+print(ctx.tf.exception)
+ctx.result2 = Cell("str")
+ctx.result2.scratch = True
+ctx.result2 = ctx.result
 ctx.compute()
-print(ctx.b.value, ctx.b.checksum)
-z = ctx.get_zip()
-print("ZIP size (should be 241):", len(z))
+print(ctx.result2.value)
+print(ctx.result2.checksum)
---
+++ tests/highlevel/scratch.sh
@@ -0,0 +1,9 @@
+python scratch-followup0.py
+echo
+python scratch.py
+echo
+python scratch-followup0.py
+echo
+python scratch-followup.py
+echo
+python scratch-followup0.py
