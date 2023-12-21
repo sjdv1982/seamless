002584dae56cd7d144a15e164537344ea3c14604
Sjoerd de Vries <sjdv1982@gmail.com>
2023-12-21 21:32:41 +0100

    minor fix

--- seamless/core/direct/run.py
+++ seamless/core/direct/run.py
@@ -343,10 +343,10 @@ def run_transformation_dict_in_process(transformation_dict, tf_checksum, tf_dund
     result_buffer = serialize(result, output_celltype)
     result_checksum = calculate_checksum(result_buffer)
 
-    print("DONE", tf_checksum.hex(), result_checksum.hex())
     database.set_transformation_result(tf_checksum, result_checksum)
     if not scratch:
         buffer_cache.guarantee_buffer_info(result_checksum, output_celltype, sync_to_remote=True)
+        buffer_cache.cache_buffer(result_checksum, result_buffer)
         buffer_remote.write_buffer(result_checksum, result_buffer)
 
     return result_checksum
