a95b55a68bc747ca8aa1661037497ef4981d695e
Sjoerd de Vries <sjdv1982@gmail.com>
2024-03-09 01:06:44 +0100

    temporary workaround for #249

--- seamless/highlevel/Transformer.py
+++ seamless/highlevel/Transformer.py
@@ -199,8 +199,8 @@ class Transformer(Base, HelpMixin):
         self.result.celltype = result_celltype
         for pin, buffer in buffers.items():
             setattr(self, pin, None)
-            getattr(self.pins, pin).celltype = "bytes"
-            setattr(self, pin, buffer)
+            getattr(self.pins, pin).celltype = "text"
+            setattr(self, pin, buffer.decode())
         self.code = bashcode
         return self
 
