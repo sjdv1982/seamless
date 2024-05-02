bac7caa9ef2bff4ce50c7b789c5fcd2bda5d2e75
Sjoerd de Vries <sjdv1982@gmail.com>
2024-05-02 13:07:22 +0200

    brush up highlink-cpp test

--- tests/highlevel/highlink-cpp.py
+++ tests/highlevel/highlink-cpp.py
@@ -1,6 +1,7 @@
 import seamless
 seamless.delegate(False)
 from seamless.highlevel import Context, Cell, Transformer
+import numpy as np
 
 ctx = Context()
 def calc_arr(period, npoints):
@@ -21,7 +22,7 @@ ctx.calc_arr.example.npoints = 200
 
 c = ctx.calc_arr_schema = Cell()
 c.celltype = "plain"
-c.mount("/tmp/calc_arr_schema.json")
+c.mount("/tmp/calc_arr_schema.json", authority="cell")
 ctx.link(c, ctx.calc_arr.schema)
 
 ctx.arr = ctx.calc_arr
@@ -37,13 +38,13 @@ plt.show()
 
 ctx.tf = lambda arr, fac, offset: 42
 ctx.tf.arr = ctx.arr
-ctx.tf.fac = 3
-ctx.tf.offset = 12
+ctx.tf.fac = 0.001
+ctx.tf.offset = 50
 ctx.tf.language = "cpp"
 ctx.compute()
 ctx.tf.example.set({})
-ctx.tf.example.fac = 3
-ctx.tf.example.offset = 12
+ctx.tf.example.fac = 3.0
+ctx.tf.example.offset = 12.0
 ctx.compute()
 
 print("\nSTEP 1\n")
@@ -55,11 +56,11 @@ ctx.header.celltype = "text"
 
 ctx.tf_schema = Cell()
 ctx.tf_schema.celltype = "plain"
-ctx.tf_schema.mount("/tmp/schema.json")
+ctx.tf_schema.mount("/tmp/schema.json", authority="cell")
 ctx.link(ctx.tf_schema, ctx.tf.schema)
 ctx.tf_result_schema = Cell()
 ctx.tf_result_schema.celltype = "plain"
-ctx.tf_result_schema.mount("/tmp/result_schema.json")
+ctx.tf_result_schema.mount("/tmp/result_schema.json", authority="cell")
 ctx.link(ctx.tf_result_schema, ctx.tf.result.schema)
 ctx.compute()
 
@@ -69,7 +70,8 @@ print("\nSTEP 2\n")
 ctx.compute()
 print(ctx.tf.status)
 print(ctx.tf.exception)
-ctx.tf.result.example.set(0)
+ctx.tf.result.example.set(np.zeros(10))
+ctx.tf.result.schema["form"]["shape"] = [[0, 100000]]   # maximum result size
 
 print("\nSTEP 3\n")
 ctx.compute()
@@ -77,4 +79,55 @@ print(ctx.tf.status)
 print(ctx.tf.exception)
 print(ctx.header.value)
 
+ctx.tf.code.mount("/tmp/code.cpp", authority="cell")
+ctx.compute()
 ### Continue in interactive mode...
+
+
+# Possible code: paste into /tmp/code.cpp
+
+"""
+typedef struct ArrStruct {
+  const double *data;
+  unsigned int shape[1];
+} ArrStruct;
+
+typedef struct ResultStruct {
+  double *data;
+  unsigned int shape[1];
+} ResultStruct;
+
+extern "C" int transform(const ArrStruct* arr, double fac, double offset, ResultStruct *result) {
+    unsigned int npoints = arr->shape[0];
+    result->shape[0] = npoints;
+    const double *input = arr->data;
+    double *output = result->data;
+    for (int n = 0; n < npoints; n++) {
+        output[n] = input[n] + fac * n + offset;
+    }
+    return 0;
+}
+"""
+
+# Possible follow-up: open /tmp/plot.png
+
+"""
+def get_plot(arr):
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from io import BytesIO
+    plt.plot(arr)
+    result = BytesIO()
+    plt.savefig(result)
+    return result.getvalue()
+
+ctx.get_plot = get_plot
+ctx.result = ctx.tf.result
+ctx.get_plot.arr = ctx.result
+ctx.plot = ctx.get_plot.result
+ctx.plot.celltype = "bytes"
+ctx.plot.mount("/tmp/plot.png", mode="w")
+ctx.compute()
+"""
+
+# Possible follow-up (2): open /tmp/schema.json and add ' "minimum": 0.002 ' for fac
